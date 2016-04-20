a2b4e739185d3fddd7ee83dbeeee1ed22781fb1b
Jennifer Lin <jenniferlin@Jennifers-MacBook-Pro.local>
2016-04-20 12:20:17 -0700

    commented

--- lab3/fft_convolve_cuda.cu
+++ lab3/fft_convolve_cuda.cu
@@ -37,28 +37,14 @@ void
 cudaProdScaleKernel(const cufftComplex *raw_data, const cufftComplex *impulse_v, 
     cufftComplex *out_data,
     int padded_length) {
-
-
-    /* TODO: Implement the point-wise multiplication and scaling for the
-    FFT'd input and impulse response. 
-
-    Recall that these are complex numbers, so you'll need to use the
-    appropriate rule for multiplying them. 
-
-    Also remember to scale by the padded length of the signal
-    (see the notes for Question 1).
-
-    As in Assignment 1 and Week 1, remember to make your implementation
-    resilient to varying numbers of threads.
-
-    */
-
     uint thread_index = blockIdx.x * blockDim.x + threadIdx.x;
     while (thread_index < padded_length) {
         cufftComplex impulse = impulse_v[thread_index];
         cufftComplex raw = raw_data[thread_index];
-        out_data[thread_index].x = (impulse.x * raw.x - impulse.y * raw.y)/padded_length;
-        out_data[thread_index].y = (impulse.y * raw.x + impulse.x * raw.y)/padded_length;
+        cufftComplex out;
+        out.x = (impulse.x * raw.x - impulse.y * raw.y)/padded_length;
+        out.y = (impulse.y * raw.x + impulse.x * raw.y)/padded_length;
+        out_data[thread_index] = out;
         thread_index += blockDim.x * gridDim.x;
     }
 }
@@ -70,71 +56,46 @@ __device__ float complexCompare(cufftComplex num1, cufftComplex num2){
     return (mag1 >= mag2) ? mag1 : mag2;
 }
 
-/*
-__device__ void warpMax(volatile float* sdata, int tid){
 
-
-}*/
-
-
-
+/**
+ * cudaMaximumKernel optimize maximum finding with reduction
+ *
+ * Each block handle block * 2 size of out_data. The reduction
+ * is performed in shared data s_max_data, on each block of data.
+ * First, load the max of magnitude of out_data[i] and out_data[i + blockDim.x]
+ * to the shared memory. Each thread read from out_data consecutavely. Thus, gloval
+ * memory is coalesced. Each thread store the 4 bybte data in s_max_data[threadId].
+ * Thus, within a warp, each thread, assigned a different bank. There is no
+ * bank conflict.
+ * Next, use a for loop to reduce the size of share memory.
+ * In each, each thread access the shared memory sequentially. Thus,
+ * thre is no bank conflict. In addition, note that the thread is used
+ * sequentially as well.
+ * When the reduction reach below 32 threads, I unroll the for loop. Since
+ * all threads in a warp is syncronize, we don't need to sync threads between
+ * each step.
+ * The reduction will find the local maximum within a block. Every block will
+ * perform atomic compare and swap on the global variable max_abs_val to get the
+ * global max.
+ */
 
 __global__
 void cudaMaximumKernel(cufftComplex *out_data, float *max_abs_val,
     int padded_length) {
 
-    /* TODO 2: Implement the maximum-finding and subsequent
-    normalization (dividing by maximum).
-
-    There are many ways to do this reduction, and some methods
-    have much better performance than others. 
-
-    For this section: Please explain your approach to the reduction,
-    including why you chose the optimizations you did
-    (especially as they relate to GPU hardware).
-
-    You'll likely find the above atomicMax function helpful.
-    (CUDA's atomicMax function doesn't work for floating-point values.)
-    It's based on two principles:
-        1) From Week 2, any atomic function can be implemented using
-        atomic compare-and-swap.
-        2) One can "represent" floating-point values as integers in
-        a way that preserves comparison, if the sign of the two
-        values is the same. (see http://stackoverflow.com/questions/
-        29596797/can-the-return-value-of-float-as-int-be-used-to-
-        compare-float-in-cuda)
-    */
-
     extern __shared__ float s_max_data[];
 
     uint threadId = threadIdx.x;
     uint i = blockIdx.x * (blockDim.x * 2)+ threadIdx.x;
-    //printf("padded_length = %d\n", padded_length);
     while((i + blockDim.x) < padded_length){
         s_max_data[threadId] = complexCompare(out_data[i], out_data[i + blockDim.x]);
         __syncthreads();
-        //printf("first copy\n");
         for(unsigned int s = blockDim.x/2; s > 0; s >>= 1){
             if (threadId < s){
                 s_max_data[threadId] = (s_max_data[threadId] >= s_max_data[threadId + s])\
                         ? s_max_data[threadId] : s_max_data[threadId + s];
+                __syncthreads();
             }
-            __syncthreads();
-        }
-        if (threadId < 32){
-            s_max_data[threadId] = (s_max_data[threadId] >= s_max_data[threadId + 32]) \
-                    ? s_max_data[threadId] : s_max_data[threadId + 32];
-            s_max_data[threadId] = (s_max_data[threadId] >= s_max_data[threadId + 16]) \
-                    ? s_max_data[threadId] : s_max_data[threadId + 16];
-            s_max_data[threadId] = (s_max_data[threadId] >= s_max_data[threadId + 8]) \
-                    ? s_max_data[threadId] : s_max_data[threadId + 8];
-            s_max_data[threadId] = (s_max_data[threadId] >= s_max_data[threadId + 4]) \
-                    ? s_max_data[threadId] : s_max_data[threadId + 4];
-            s_max_data[threadId] = (s_max_data[threadId] >= s_max_data[threadId + 2]) \
-                    ? s_max_data[threadId] : s_max_data[threadId + 2];
-            s_max_data[threadId] = (s_max_data[threadId] >= s_max_data[threadId + 1]) \
-                    ? s_max_data[threadId] : s_max_data[threadId + 1];
-
         }
         if (threadId == 0){
             atomicMax(max_abs_val, s_max_data[0]);
@@ -143,6 +104,12 @@ void cudaMaximumKernel(cufftComplex *out_data, float *max_abs_val,
     }
 }
 
+/**
+ * cudaDivideKernal load the outdata to local register first,
+ * perform all the necessary calculation and write it back into
+ * global memory. Note that all memory access is sequential so glbal
+ * memory read/write is coalesced.
+ */
 __global__
 void
 cudaDivideKernel(cufftComplex *out_data, float *max_abs_val,
@@ -174,7 +141,6 @@ void cudaCallMaximumKernel(const unsigned int blocks,
         float *max_abs_val,
         const unsigned int padded_length) {
         cudaMaximumKernel<<<blocks, threadsPerBlock, sizeof(float)*threadsPerBlock>>>(out_data, max_abs_val, padded_length);
-    /* TODO 2: Call the max-finding kernel. */
 
 }
 
@@ -184,6 +150,6 @@ void cudaCallDivideKernel(const unsigned int blocks,
         cufftComplex *out_data,
         float *max_abs_val,
         const unsigned int padded_length) {
-        cudaDivideKernel<<<blocks, threadsPerBlock>>>(out_data, max_abs_val, padded_length);
-    /* TODO 2: Call the division kernel. */
+    cudaDivideKernel << < blocks, threadsPerBlock >> > (out_data, max_abs_val, padded_length);
+
 }
